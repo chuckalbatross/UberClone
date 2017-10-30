07523a39a26c46b4e95b407f45dcb71b40d09b18
James Bonner <jbonner89@gmail.com>
2017-10-30 13:43:22 -0700

    (feat) Add Sequelize connection

--- database/index.js
+++ database/index.js
@@ -1,18 +1,32 @@
-var mysql = require('mysql');
-
+const mysql = require('mysql');
+const Sequelize = require('sequelize');
+  
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "trip".
 
-exports.connection = mysql.createConnection({
+const connection = mysql.createConnection({
   user: 'root',
   password: ''
 });
 
-exports.connection.connect(err => {
+connection.connect(err => {
   if (err) {
     console.log(`Error! Connection to bookingsDB unsuccessful: ${err}`);
   } else {
     console.log(`Success! Connection to bookingsDB established`);
   }
 })
+
+const sequelize = new Sequelize('bookings', 'root', null, {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Success! Sequelize connection established');
+  })
+  .catch(err => {
+    console.log(`Error! Sequelize unable to connect to database: ${err}`);
+  });
