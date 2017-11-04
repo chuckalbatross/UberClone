253b0808c3703e88e29e3b06481cba8344074516
James Bonner <jbonner89@gmail.com>
2017-11-04 14:25:07 -0700

    (fix) Cleanup driver-simulation-load-test.js

--- DriverSimulation/driver-simulation-load-test.js
+++ DriverSimulation/driver-simulation-load-test.js
@@ -3,15 +3,6 @@ const Chance = require('chance');
 
 var chance = new Chance();
 
-
-////////////////////////////////////////////////////////////////
-// TESTING
-////////////////////////////////////////////////////////////////
-
-//now instantiate 20k drivers
-
-// var testDriver = new Driver('b55e0217-d995-5e27-a19a-a007773b9092', 37.5875953, -122.4277966);
-
 // CREATE 20K DRIVERS
 var driverArr = [];
 var testDriver;
@@ -22,37 +13,6 @@ for (var i = 0; i < 20000; i++) {
     chance.longitude({fixed: 7, min: -122.4779892, max: -122.4144744}));
   driverArr.push(testDriver);
 }
-
-// NOT REALISTIC: all transmitting state at same small period
-// driverArr.forEach(driver => {
-//   // console.log(driver);
-//   driver.transmitState();
-// });
-
-
-// transmitStateArr = [];
-// var randNum;
-// HAHAHA really fast at beginning (because any random number will do because none are taken), then gets slower and slower for last couple
-// while (transmitStateArr.length < 20000) {
-//   // generate a random number between [0, 20000)
-//   randNum = Math.floor(Math.random() * 20000);
-//   // if transmitStateArr doesn't contain randNum
-//   if (!transmitStateArr.includes(randNum)) {
-//     // call transmit state that driver at randNum index
-//     driverArr[randNum].transmitState();
-//     // push randNum into transmitStateArr
-//     transmitStateArr.push(randNum);
-//   }
-// }
-
-
-// DOESN'T WORK: memory overflow
-// var index = 0;
-// while (index < 10) {
-//   setTimeout(() => {
-//     driverArr[index++].transmitState();
-//   }, 10)
-// }
 
 // CALL TRANSMIT LOCATION EVENLY OVER TIME
 var setIntervalX = function(callback, delay, repetitions) {
@@ -70,8 +30,6 @@ var setIntervalX = function(callback, delay, repetitions) {
 setIntervalX((x) => {
   driverArr[x].transmitState();
 }, 10, 20000);
-
-
 
 // GENERATE 20K TRIPS AND GIVE EACH DRIVER ONE
 var tripsGenerator = function() {
@@ -99,7 +57,6 @@ var tripsGenerator = function() {
 
     return trip;
 }
-  
 
 for (var i = 0; i < 20000; i++) {
 
@@ -110,24 +67,3 @@ for (var i = 0; i < 20000; i++) {
   driverArr[i].dropoffRider(trip.pickup, trip.dropoff);
 
 }
-
-
-// testDriver.transmitState();
-
-// // also needs tripID
-// var pickupObj = {
-//   lat: 37.5906004,
-//   long: -122.4402796,
-//   est: 1
-// }
-// var dropoffObj = {
-//   lat: 37.5660525,
-//   long: -122.4216035,
-//   est: 5
-// }
-
-// testDriver.dropoffRider(pickupObj, dropoffObj);
-
-
-
-
